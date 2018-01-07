b62f33bc39c61510a752c354fb5140fa85d4e3bd
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-01-07 11:39:47 +0100

    add Main.jsx

---
+++ src/js/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import ??? from '../js/???.jsx';
+require('../sass/main.scss');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div>
+					
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+		<Main />, document.getElementById('app'));
+		
+});
